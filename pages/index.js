03d788053777db0a7677846a6e94e73d562d7878
lan666as <4zidan@gmail.com>
2022-09-30 05:31:31 +0700

    fix responsive issue

--- pages/index.js
+++ pages/index.js
@@ -104,7 +104,7 @@ export default function Home() {
         </section> */}
 
         <section className="h-[50vh] my-[25vh] relative">
-          <div className="absolute z-[1] w-[50%] top-1/2 translate-y-[-50%]">
+          <div className="absolute z-[1] w-[100%] xl:w-[50%] top-1/2 translate-y-[-50%]">
             <h1 className="text-[#07B0F8] text-[3rem] font-bold drop-shadow-[0_5px_10px_rgba(0,0,0,0.2)]">
               UNITREND by Institute for Policy Development
             </h1>
@@ -112,7 +112,7 @@ export default function Home() {
               Presenting Data for Better Policy
             </p>
           </div>
-          <Image src={heroImg} alt="Futuristic city" className='w-[40vw] absolute right-0 top-2/4 translate-y-[-50%] h-[auto] contrast-[110%] brightness-[70%] saturate-0'></Image>
+          <Image src={heroImg} alt="Futuristic city" className='w-[40vw] absolute right-0 top-2/4 translate-y-[-50%] h-[auto] contrast-[110%] brightness-[70%] saturate-0 hidden xl:block'></Image>
         </section>
         <section className="text-justify my-16">
           <h1 className="text-black text-[2rem] font-bold">
