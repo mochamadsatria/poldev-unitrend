499be0df16f87dd1ca6e1942c3bbb5126c26dd59
Ahmad Zidan <4zidan@gmail.com>
2022-11-02 22:20:32 +0700

    fix bug

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   swcMinify: true,
   experimental: { images: { allowFutureImage: true } },
   output: 'standalone',
+  env: {
+    SUPERSET_HOST: process.env.SUPERSET_HOST,
+    SUPERSET_DOMAIN: process.env.SUPERSET_DOMAIN,
+    SUPERSET_USERNAME: process.env.SUPERSET_USERNAME,
+    SUPERSET_PASSWORD: process.env.SUPERSET_PASSWORD,
+    SUPERSET_DASHBOARD_ID: process.env.SUPERSET_DASHBOARD_ID,
+  },
 }
 
 module.exports = nextConfig
