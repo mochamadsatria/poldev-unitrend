83717d1a7b584db6a3e0721a39d35f89a52f248b
Ahmad Zidan <4zidan@gmail.com>
2022-11-30 21:38:54 +0700

    fix issues

--- utility/scrape_economic_development.sh
+++ utility/scrape_economic_development.sh
@@ -11,16 +11,23 @@
 # systemctl restart cron
 
 ROOT="/home/poldev/work/poldev-unitrend"
+UPDATE_OTHER=0
 
 cd $ROOT
 mkdir -p assets/data
+if [[ ! -f assets/data/Data_Pemilu.json ]]
+then
+    UPDATE_OTHER=1
+fi
 
 cd $ROOT/utility
 gdown --folder https://drive.google.com/drive/folders/1KBz0MBHf7BcK9EQCNK4szh1tYG_Z7GXf
 python scrape_economic_development.py
+if [ $UPDATE_OTHER = 1]; then
+    python scrape_indonesian_election.py
+fi
 
 cd $ROOT
-
 # git add .
 # git commit -m "update economic development data"
 # git pull
