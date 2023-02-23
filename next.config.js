1b34c747b93d7fc43a2da82f466e9da5a993b495
mochamadsatria <mochamadsatriarizapermana@gmail.com>
2023-02-23 15:14:05 +0700

    1

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  //swcMinify: true,
   experimental: { images: { allowFutureImage: true } },
-  output: 'standalone',
-  publicRuntimeConfig: {
+  output: "standalone",
+  /*publicRuntimeConfig: {
     SUPERSET_HOST: process.env.SUPERSET_HOST,
     SUPERSET_DOMAIN: process.env.SUPERSET_DOMAIN,
     SUPERSET_DASHBOARD_ID: process.env.SUPERSET_DASHBOARD_ID,
@@ -12,7 +12,7 @@ const nextConfig = {
   serverRuntimeConfig: {
     SUPERSET_USERNAME: process.env.SUPERSET_USERNAME,
     SUPERSET_PASSWORD: process.env.SUPERSET_PASSWORD,
-  }
-}
+  }*/
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
